10ff922d42eb69aec904bd081868352a81f6d157
Morpheusou <feng.ou.ipad@gmail.com>
2014-11-05 09:45:21 -0500

    add

---
+++ demo/retType.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int f1()
+{
+	cout << " I am f function" << endl;
+}
+
+int f2()
+{
+	cout << "I am f function without return" << endl;
+}
+
+f3()
+{
+	cout << "I am f function with return" << endl;
+	return 0;
+}
+
+int main()
+{
+	f1();
+	f2();
+	f3();
+	return 0;
+}
