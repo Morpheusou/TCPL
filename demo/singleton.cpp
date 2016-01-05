fc35cb19fd4277fe2aa5ae089e01a98b375ff06d
Morpheusou <morpheusou@hotmail.com>
2016-01-05 14:17:24 +0800

    singleton

--- demo/singleton.cpp
+++ demo/singleton.cpp
@@ -5,7 +5,9 @@ class singleton
 {
 public:
 	static singleton *Instance() { return &_instance;}
+	int GetCount() { return _refCount; }
 private:
+	int _refCount;
 	static singleton _instance;
 };
 
@@ -13,9 +15,11 @@ private:
 class singleton
 {
 public:
-	singleton& Instance() {return _instance;}
+	static singleton& Instance() {return _instance;}
+	int GetCount() { return _refCount; }
 private:
-	static const int _refCount = 100;
+	static const int _maxRefCount = 100;
+	int _refCount;
 	static singleton _instance;
 };
 
@@ -23,8 +27,10 @@ private:
 class singleton
 {
 public:
-	singleton Instance() { return _instance; }
+	static singleton Instance() { return _instance; }
+	int getCount() { return _refCount; }
 private:
+	int _refCount;
 	static singleton _instance;
 };
 
@@ -32,12 +38,45 @@ private:
 class singleton
 {
 public:
-	singleton Instance() { return _instance; }
+	static singleton Instance() { return _instance; }
+	int getCount() { return _refCount; }
 private:
+	int _refCount;
 	singleton _instance;
 };
 
+//----------E---------------
+class singleton
+{
+public:
+	static singleton *Instance() { 
+		_instance = new singleton;
+	}
+private:
+	int _refCount;
+	singleton * _instance;
+};
+
+//----------F---------------
+class singleton
+{
+public:
+	static singleton *Instance() {
+		if (_instance == 0) {
+			_instance = new singleton;
+			_refCount = 1;
+		} else {
+			++_refCount;
+		}
+	}
+private:
+	int _refCount;
+	static singleton * _instance;
+};
+singleton * singleton::_instance = 0;
+
 int main()
 {
+	int count = singleton::Instance()->GetCount();
 	return 0;
 }
